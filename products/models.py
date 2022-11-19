70514bb02ab0140d46f9313bdba699b2126a7695
mohsen <mohzia1380@gmail.com>
2022-11-20 01:35:06 +0330

    create models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Category(models.Model):
+    parent = models.ForeignKey('selft', verbose_name=_('parent'), blank=True, null=True, on_delete=models.CASCADE())
+    title = models.CharField(_('title'), max_length=50)
+    description = models.TextField(_('description'), blank=True)
+    avatar = models.ImageField(_('avatar'), blank=True, upload_to='categories')
+    is_enable = models.BooleanField(_('is enable'), default=True)
+    created_time = models.DateTimeField(_('created time'), auto_now_add=True)
+    updated_time = models.DateTimeField(_('updated time'), auto_now=True)
+
+    class Meta:
+        db_table = 'categories'
+        verbose_name = _('Category')
+        verbose_name_plural = _('Categories')
+
+class Product(models.Model):
+    title = models.CharField(_('title'),max_length=50)
+    description = models.TextField(_('description'), blank=True)
+    avatar = models.ImageField(_('avatar'), blank=True, upload_to='categories')
+    is_enable = models.BooleanField(_('is enable'), default=True)
+    categories = models.ManyToManyField('Category', verbose_name=_('category'))
+    created_time = models.DateTimeField(_('created time'), auto_now_add=True)
+    updated_time = models.DateTimeField(_('updated time'), auto_now=True)
+
+    class Meta:
+        db_table = 'products'
+        verbosed_name = _('product')
+        verbosed_name_plural = _('products')
+
+
+class File(models.Model):
+    product = models.ForeignKey('Product',verbose_name=_('product'),on_delete=models.CASCADE)
+    title = models.CharField(_('title'),max_length=50)
+    is_enable = models.BooleanField(_('is enable'), default=True)
+    created_time = models.DateTimeField(_('created time'), auto_now_add=True)
+    updated_time = models.DateTimeField(_('updated time'), auto_now=True)
+
+    class Meta:
+        db_table = 'files'
+        verbosed_name = _('file')
+        verbosed_name_plural = _('files')
