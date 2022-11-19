a1cc900a6153132957f32fca93728e17553fe597
mohsen <mohzia1380@gmail.com>
2022-11-20 02:07:41 +0330

    fix bug & migrations & innstall app in setting

--- products/models.py
+++ products/models.py
@@ -1,45 +1,50 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+
 
 
 class Category(models.Model):
-    parent = models.ForeignKey('selft', verbose_name=_('parent'), blank=True, null=True, on_delete=models.CASCADE())
-    title = models.CharField(_('title'), max_length=50)
-    description = models.TextField(_('description'), blank=True)
-    avatar = models.ImageField(_('avatar'), blank=True, upload_to='categories/')
-    is_enable = models.BooleanField(_('is enable'), default=True)
-    created_time = models.DateTimeField(_('created time'), auto_now_add=True)
-    updated_time = models.DateTimeField(_('updated time'), auto_now=True)
+    parent = models.ForeignKey('self', verbose_name='parent', blank=True, null=True, on_delete=models.CASCADE)
+    title = models.CharField('title', max_length=50)
+    description = models.TextField('description', blank=True)
+    avatar = models.ImageField('avatar', blank=True, upload_to='categories/')
+    is_enable = models.BooleanField('is enable', default=True)
+    created_time = models.DateTimeField('created time', auto_now_add=True)
+    updated_time = models.DateTimeField('updated time', auto_now=True)
 
     class Meta:
         db_table = 'categories'
-        verbose_name = _('Category')
-        verbose_name_plural = _('Categories')
+        ordering = ('-created_time',)
+
+    def __str__(self):
+        return self.title
 
 class Product(models.Model):
-    title = models.CharField(_('title'),max_length=50)
-    description = models.TextField(_('description'), blank=True)
-    avatar = models.ImageField(_('avatar'), blank=True, upload_to='avatar/')
-    is_enable = models.BooleanField(_('is enable'), default=True)
-    categories = models.ManyToManyField('Category', verbose_name=_('category'))
-    created_time = models.DateTimeField(_('created time'), auto_now_add=True)
-    updated_time = models.DateTimeField(_('updated time'), auto_now=True)
+    title = models.CharField('title',max_length=50)
+    description = models.TextField('description', blank=True)
+    avatar = models.ImageField('avatar', blank=True, upload_to='avatar/')
+    is_enable = models.BooleanField('is enable', default=True)
+    categories = models.ManyToManyField('Category', verbose_name='category')
+    created_time = models.DateTimeField('created time', auto_now_add=True)
+    updated_time = models.DateTimeField('updated time', auto_now=True)
 
     class Meta:
         db_table = 'products'
-        verbosed_name = _('product')
-        verbosed_name_plural = _('products')
+        ordering = ('-created_time',)
 
+    def __str__(self):
+        return self.title
 
 class File(models.Model):
-    product = models.ForeignKey('Product',verbose_name=_('product'),on_delete=models.CASCADE)
-    title = models.CharField(_('title'),max_length=50)
-    file = models.FileField(_('file'), upload_to='files/%Y/%m/%d/')
-    is_enable = models.BooleanField(_('is enable'), default=True)
-    created_time = models.DateTimeField(_('created time'), auto_now_add=True)
-    updated_time = models.DateTimeField(_('updated time'), auto_now=True)
+    product = models.ForeignKey('Product',verbose_name='product',on_delete=models.CASCADE)
+    title = models.CharField('title',max_length=50)
+    file = models.FileField('file', upload_to='files/%Y/%m/%d/')
+    is_enable = models.BooleanField('is enable', default=True)
+    created_time = models.DateTimeField('created time', auto_now_add=True)
+    updated_time = models.DateTimeField('updated time', auto_now=True)
 
     class Meta:
         db_table = 'files'
-        verbosed_name = _('file')
-        verbosed_name_plural = _('files')
+        ordering = ('-created_time',)
+
+    def __str__(self):
+        return self.title
