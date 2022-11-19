8ae68ffe83debb60e242abcbe88e7eb36995bced
mohsen <mohzia1380@gmail.com>
2022-11-20 03:15:35 +0330

    add view get all product

--- products/serializers.py
+++ products/serializers.py
@@ -4,7 +4,7 @@ from  .models import *
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ('title', 'desciption' , 'avatar')
+        fields = ('title', 'description' , 'avatar')
 
 class FileSerializer(serializers.ModelSerializer):
     class Meta:
--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views
+from .views import *
 
 app_name='products'
 urlpatterns = [
-    path('products/',ProductListView.as_view()),
+    path('',ProductListView.as_view()),
 ]
