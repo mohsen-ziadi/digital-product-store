ef0615d17bf606f951b97f8318bfe050679bed8a
mohsen <mohzia1380@gmail.com>
2022-11-20 02:51:08 +0330

    add serializer

---
+++ products/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from  .models import *
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('title', 'desciption' , 'avatar')
+
+class FileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = File
+        fields = ('title','file')
+
+class ProductSerializer(serializers.ModelSerializer):
+    categories = CategorySerializer(many=True)
+
+    class Meta:
+        model = Product
+        fields = ('title', 'description','avatar','categories')
