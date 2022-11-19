c921cf3f19b37fa67f1af7c915d4db3131fa21a6
mohsen <mohzia1380@gmail.com>
2022-11-20 03:27:27 +0330

    edit related name of file

--- products/serializers.py
+++ products/serializers.py
@@ -13,8 +13,8 @@ class FileSerializer(serializers.ModelSerializer):
 
 class ProductSerializer(serializers.ModelSerializer):
     categories = CategorySerializer(many=True)
-    file_set = FileSerializer(many=True)
+    files = FileSerializer(many=True)
 
     class Meta:
         model = Product
-        fields = ('title', 'description','avatar','categories','file_set')
+        fields = ('title', 'description','avatar','categories','files')
