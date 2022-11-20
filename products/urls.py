926fd6c2c281aa3c3040168ee8b2d86a97bc6be1
mohsen <mohzia1380@gmail.com>
2022-11-20 16:51:38 +0330

    add view to category

--- products/urls.py
+++ products/urls.py
@@ -3,7 +3,8 @@ from .views import *
 
 app_name='products'
 urlpatterns = [
-    path('',ProductListView.as_view(), name='product-list'),
-    path('<int:pk>/',ProductDetailView.as_view(),name="product-detail")
-
+    path('product/',ProductListView.as_view(), name='product-list'),
+    path('product/<int:pk>/',ProductDetailView.as_view(), name="product-detail"),
+    path('category/',CategoryListView.as_view(), name="category-list"),
+    path('category/<int:pk>/', CategoryDetailView.as_view(), name="category-detail"),
 ]
--- products/views.py
+++ products/views.py
@@ -6,12 +6,14 @@ from .models import *
 from .serializers import *
 
 
+# start productView
 class ProductListView(APIView):
 
     def get(self,request):
         products = Product.objects.all()
         serializers = ProductSerializer(products, many=True, context={'request': request})
         return Response(serializers.data)
+
 
 class ProductDetailView(APIView):
 
@@ -23,3 +25,26 @@ class ProductDetailView(APIView):
 
         serializer = ProductSerializer(product, context={'request':request})
         return Response(serializer.data)
+
+
+
+# start categoryView
+class CategoryListView(APIView):
+
+    def get(self, request):
+        categories = Category.objects.all()
+        serializers = CategorySerializer(categories, many=True, context={'request': request})
+        return Response(serializers.data)
+
+
+class CategoryDetailView(APIView):
+
+    def get(self, request, pk):
+        try:
+            category = Category.objects.get(pk=pk)
+        except Category.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = CategorySerializer(category, context={'request': request})
+        return Response(serializer.data)
+
